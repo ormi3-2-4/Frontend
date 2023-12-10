02aa9d333c6573fd61d9fc6bc8547da95705d037
UkiDelly <daehyeon1999@naver.com>
2023-12-10 23:42:14 +0900

    🚧 dev: env 로드 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logger/logger.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: 'config.env');
+
+  Logger().d(dotenv.env["MAP_API_KEY"]);
   runApp(const MyApp());
 }
 
