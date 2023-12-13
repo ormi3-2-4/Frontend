d644a664e2c7b4021b82fe0e4ae15b0f0e8dbf56
UkiDelly <daehyeon1999@naver.com>
2023-12-13 21:26:23 +0900

    ✨ add: 운동 시작 카드 구현

--- lib/screen/start/start_screen.dart
+++ lib/screen/start/start_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:ormi2_4/screen/start/widgets/start_running_card.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -8,8 +11,20 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Column()),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const Spacer(),
+              Text("런닝 메이트", style: context.textTheme.displayMedium),
+              SizedBox(height: 50.h),
+              const StartRunningCardWidget(),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
