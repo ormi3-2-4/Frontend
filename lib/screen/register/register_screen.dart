0db849677f17130afdf10c00fe3228290ced2b8a
UkiDelly <daehyeon1999@naver.com>
2023-12-13 13:41:15 +0900

    ➕ add: Register 화면 추가 및 라우트 등록

---
+++ lib/screen/register/register_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  static const routePath = '/register';
+  static const routeName = 'RegisterScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
