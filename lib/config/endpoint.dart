bc6f700a071908ee5e4a4c1bc4da0eab0db2b72a
UkiDelly <daehyeon1999@naver.com>
2023-12-20 09:46:10 +0900

    ✨ add: Record Endpoint 추가

--- lib/config/endpoint.dart
+++ lib/config/endpoint.dart
@@ -8,3 +8,11 @@ class _User {
   String get login => '$_prefix/login/';
   String get register => '$_prefix/register/';
 }
+
+class _Record {
+  final String _prefix = '/record';
+
+  String get create => '$_prefix/';
+  String get list => '$_prefix/';
+  String detail(int recordId) => '$_prefix/$recordId/';
+}
