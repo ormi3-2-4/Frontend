04bdbe2b6a140ffcb14184f9e684e0357657be2f
UkiDelly <daehyeon1999@naver.com>
2023-12-21 22:30:14 +0900

    ✨ add: Record detail 데이터 조회 함수 구현

--- lib/repository/record_repository.dart
+++ lib/repository/record_repository.dart
@@ -4,12 +4,14 @@ import 'package:ormi2_4/models/response/base_response.dart';
 import 'package:ormi2_4/models/response/error.dart';
 import 'package:ormi2_4/models/response/record_response.dart';
 
+import '../models/record_model.dart';
+
 class RecordRepository {
   const RecordRepository(this.dio);
 
   final Dio dio;
 
-  Future<BaseResponse> getRecordList() async {
+  Future<BaseResponse<RecordHistoryResponse>> getRecordList() async {
     try {
       final res = await dio.get(Endpoint.record.list);
       final responseModel = RecordHistoryResponse.fromJson(res.data);
@@ -22,5 +24,15 @@ class RecordRepository {
   Future<BaseResponse> createRecord() async {
     // TODO: Reqeust 모델 생성, 로직 구현
     return const BaseResponse.data(true);
+  }
+
+  Future<BaseResponse<Record>> getRecord(int recordId) async {
+    try {
+      final res = await dio.get(Endpoint.record.detail(recordId));
+      final responseModel = Record.fromJson(res.data);
+      return BaseResponse.data(responseModel);
+    } on DioException catch (e) {
+      return BaseResponse.error(AppError(error: e.error ?? ''));
+    }
   }
 }
