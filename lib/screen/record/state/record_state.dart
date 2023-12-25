a88a7ed85a1d456f3707e50ebfaed3ed00b86169
UkiDelly <daehyeon1999@naver.com>
2023-12-25 10:26:22 +0900

    ✨ add: Controller에 기록 생성, 수정, 삭제 기능 추가

--- lib/screen/record/state/record_state.dart
+++ lib/screen/record/state/record_state.dart
@@ -3,6 +3,11 @@ import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:logger/logger.dart';
 import 'package:ormi2_4/models/enums.dart';
+import 'package:ormi2_4/models/response/base_response.dart';
+import 'package:ormi2_4/repository/record_repository.dart';
+import 'package:ormi2_4/service/dio_service.dart';
+
+import '../../../models/record_model.dart';
 
 enum RunningState { running, paused, stopped }
 
@@ -10,10 +15,62 @@ class RecordController extends GetxController {
   static RecordController get instance => Get.find();
   // RxList<Polyline> polylines = <Polyline>[].obs;
   RxList<LatLng> coords = <LatLng>[].obs;
+
   Rx<RunningState> runningState = RunningState.running.obs;
   Rx<Kind> kind = Kind.run.obs;
   Rx<Stream<Position>> currentPositionStream = Geolocator.getPositionStream().obs;
   Rx<LatLng> currentLocation = const LatLng(0, 0).obs;
+  RxDouble speed = 0.0.obs;
+  Rx<DateTime> startAt = DateTime.now().obs;
+  RxString time = "".obs;
+  Record? _record;
+
+  Future<void> createRecord() async {
+    await DioService.instance.addJwt();
+    final res = await RecordRepository(DioService.instance.dio).createRecord();
+
+    switch (res) {
+      case BaseResponseData():
+        _record = res.data;
+        break;
+      case BaseResponseError():
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  Future<void> updateRecord() async {
+    await DioService.instance.addJwt();
+    final res =
+        await RecordRepository(DioService.instance.dio).updateRecord(_record!.id, kind.value);
+    switch (res) {
+      case BaseResponseData():
+        _record = res.data;
+        break;
+      case BaseResponseError():
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  Future<void> endRecord() async {
+    await DioService.instance.addJwt();
+    final res = await RecordRepository(DioService.instance.dio).endRecord(_record!.id, coords);
+    switch (res) {
+      case BaseResponseData():
+        _record = res.data;
+        break;
+      case BaseResponseError():
+        break;
+
+      default:
+        break;
+    }
+  }
 
   void listenCurrentPosition() {
     currentPositionStream.value.listen(
@@ -22,6 +79,10 @@ class RecordController extends GetxController {
           currentLocation.value = LatLng(position.latitude, position.longitude);
           coords.add(LatLng(position.latitude, position.longitude));
           Logger().i(LatLng(position.latitude, position.longitude));
+          speed.value = position.speed;
+
+          final diff = startAt.value.difference(position.timestamp);
+          time.value = "${diff.inMinutes} : ${diff.inSeconds % 60}";
         }
       },
     );
@@ -35,7 +96,7 @@ class RecordController extends GetxController {
     this.kind.value = kind;
   }
 
-  void onPressedEnd() {
+  Future<void> onPressedEnd() async {
     Logger().d(coords);
   }
 
